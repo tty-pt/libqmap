21de104505e67e79315b62d67b9c48bcf77abae6
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2026-08-27 21:44:36 +0100

    update gitignore

--- objects-set.mk
+++ objects-set.mk
@@ -1,2 +1,8 @@
 CFLAGS-libqmap-o := -fPIC
+CFLAGS-idm-o := -fPIC
+CFLAGS-qmap-o :=
 CFLAGS-test-o :=
+CFLAGS-test_extended-o :=
+CFLAGS-test_multivalue-o :=
+CFLAGS-test_record-o :=
+CFLAGS-bench_multivalue-o :=
